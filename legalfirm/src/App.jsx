3b5e834705ee5807815d9ff06b2d7b45061ac343
hollermay <udayanmoudgil@gmail.com>
2024-01-06 17:08:21 +0530

    Added Browser Router

--- legalfirm/src/App.jsx
+++ legalfirm/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {BrowserRouter} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,18 +8,18 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
       <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
       <Navbar/>
       <Hero/>
-      <div></div>
+      </div>
       <About />
       <Services />
       <Team />
       <Footer />
     </div>
-    </>
+    </BrowserRouter>
   )
 }
 
