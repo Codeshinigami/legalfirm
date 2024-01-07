3be9aaf6d57de48407a4d121332d8b8fd39fb937
Varinder Dhillon <98728916+Varinder-Dhillon0@users.noreply.github.com>
2024-01-07 22:20:05 +0530

    Updated Navigations
    changelog
    - added popular plan in plans
    - added highlight for opened path
    - updated navigation links for dashboard sidebar

--- legalfirm/src/Pages/Plans.jsx
+++ legalfirm/src/Pages/Plans.jsx
@@ -3,38 +3,41 @@ import { check } from "../assets";
 const Pricing_categories = [
     {
         name: "Free",
-        desc: "Base plan for everyone.",
+        desc: "The essentials to provide your best work for clients.",
         popular: false,
-        price: "15",
+        price: "0",
         facilities: ["5 products", "Up to 1,000 subscribers", "Basic analytics", "48-hour support response time"]
     },
     {
-        name: "Free",
-        desc: "Base plan for everyone.",
+        name: "Premium",
+        desc: "A plan that scales with your rapidly growing business.",
         popular: true,
-        price: "15",
+        price: "9.99",
         facilities: ["5 products", "Up to 1,000 subscribers", "Basic analytics", "48-hour support response time"]
     },
     {
-        name: "Free",
-        desc: "Base plan for everyone.",
+        name: "Business",
+        desc: "Dedicated support and infrastructure for your company.",
         popular: false,
-        price: "15",
+        price: "29.99",
         facilities: ["5 products", "Up to 1,000 subscribers", "Basic analytics", "48-hour support response time"]
     }
 ]
 
 export default function Plans() {
     return (
-        <div className="flex w-[100%] justify-between pl-10 pr-10 align-middle mt-20">
+        <div className="flex w-[100%] justify-between items-center pl-10 pr-10 align-middle mt-16">
             {Pricing_categories.map((pricing, i) => {
                 const { name, desc, popular, price, facilities } = pricing;
                 return (
-                    <div key={i} className="h-[500px] w-[32%] p-8 rounded-2xl border-[1px] border-gray-300">
-                        <h2 className="font-semibold text-xl">{name}</h2>
+                    <div key={i} className={"h-[500px] w-[32%] p-8 rounded-2xl border-[1px] border-gray-300 " + (popular ? "border-blue-600 border-[2px] h-[530px] w-[35%]" : "")}>
+                        <div className="flex items-center justify-between">
+                            <h2 className="font-semibold text-xl">{name}</h2>
+                            {popular && <p className="text-xs font-semibold bg-[#EDECFC] text-indigo-600 p-[7px] rounded-full">Most Popular</p>}
+                        </div>
                         <p className="pt-1 pb-3 text-sm">{desc}</p>
                         <h3 className="pt-3 pb-3 text-4xl font-bold">${price} <span className="text-sm font-semibold text-gray-500">/month</span></h3>
-                        <button className="w-[100%] mt-3 text-[15px] text-blue-600 font-semibold rounded border-[1px] border-blue-100 border-solid p-1 mb-7">Buy Plan</button>
+                        <button className={"w-[100%] mt-3 text-[15px] text-blue-600 font-medium rounded border-[1px] border-blue-100 border-solid p-1 mb-7"  + (popular ? " !text-white bg-blue-600" : "")}>Buy Plan</button>
                         <ul>
                             {facilities.map((facility, index) => {
                                 return (
--- legalfirm/src/layout/dashboard/sidebar.jsx
+++ legalfirm/src/layout/dashboard/sidebar.jsx
@@ -1,6 +1,16 @@
 import { swoc } from "../../assets"
+import { useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 export default function Sidebar() {
+
+    const location = useLocation();
+    const [currentPath, setCurrentPath] = useState(location.pathname)
+
+    useEffect(() =>{
+        setCurrentPath(location.pathname);
+    },[location]);
+
     return (
         <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
             <div className="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
@@ -16,7 +26,7 @@ export default function Sidebar() {
                             </div>
                         </li>
                         <li>
-                            <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                            <a href="/dashboard" className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${currentPath == "/dashboard" && "bg-gray-5 text-gray-800 border-l-4 border-transparent border-indigo-500 pr-6"}`}>
                                 <span className="inline-flex justify-center items-center ml-4">
                                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                                 </span>
@@ -24,7 +34,7 @@ export default function Sidebar() {
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                            <a href="/plans" className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${currentPath == "/plans" && "bg-gray-5 text-gray-800 border-l-4 border-transparent border-indigo-500 pr-6"}`}>
                                 <span className="inline-flex justify-center items-center ml-4">
                                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path></svg>
                                 </span>
@@ -38,7 +48,7 @@ export default function Sidebar() {
                             </div>
                         </li>
                         <li>
-                            <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                            <a href="/legalsage" className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${currentPath == "/legalsage" && "bg-gray-5 text-gray-800 border-l-4 border-transparent border-indigo-500 pr-6"}`}>
                                 <span className="inline-flex justify-center items-center ml-4">
                                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
                                 </span>
@@ -46,7 +56,7 @@ export default function Sidebar() {
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                            <a href="/generatedocs" className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${currentPath == "/generatedocs" && "bg-gray-5 text-gray-800 border-l-4 border-transparent border-indigo-500 pr-6"}`}>
                                 <span className="inline-flex justify-center items-center ml-4">
                                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
                                 </span>
@@ -60,7 +70,7 @@ export default function Sidebar() {
                             </div>
                         </li>
                         <li>
-                            <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                            <a href="/profile" className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${currentPath == "/profile" && "bg-gray-5 text-gray-800 border-l-4 border-transparent border-indigo-500 pr-6"}`}>
                                 <span className="inline-flex justify-center items-center ml-4">
                                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
                                 </span>
@@ -68,7 +78,7 @@ export default function Sidebar() {
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                            <a href="/settings" className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${currentPath == "/settings" && "bg-gray-5 text-gray-800 border-l-4 border-transparent border-indigo-500 pr-6"}`}>
                                 <span className="inline-flex justify-center items-center ml-4">
                                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
@@ -79,7 +89,7 @@ export default function Sidebar() {
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                            <a href="/logout" className={`relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6 ${currentPath == "/logout" && "bg-gray-5 text-gray-800 border-l-4 border-transparent border-indigo-500 pr-6"}`}>
                                 <span className="inline-flex justify-center items-center ml-4">
                                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
                                 </span>
