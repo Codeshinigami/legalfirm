2c49bcbeb563259ec40568d9e35981470652d7d6
Varinder Dhillon <98728916+Varinder-Dhillon0@users.noreply.github.com>
2024-01-13 22:42:53 +0530

    Signin page
    changelog-
    - toglle b/w signup and signin added
    - login button on landing page

--- client/src/layout/landing/Navbar.jsx
+++ client/src/layout/landing/Navbar.jsx
@@ -7,10 +7,11 @@ import SignIn from '../../components/signIn.jsx';
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [login, setLogin] = useState(false);
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-[#ffffff] `}>
-      <SignIn/>
+      {login && <SignIn setLogin={setLogin}/>}
       <div className=" w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to="/"
           className="flex items-center gap-2"
@@ -31,6 +32,7 @@ const Navbar = () => {
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
+          <li className='text-[18px] font-medium cursor-pointer'><button className=" list-none text-violet-600 text-center text-[18px] shadow-lg border-violet-600 border bg-white self-center justify-center px-8 py-1 rounded-[40px] max-md:px-5" onClick={() => setLogin(!login)}>Login</button></li>
           <li className='text-[18px] font-medium cursor-pointer'><button className=" list-none text-violet-600 text-center text-[18px] shadow-lg border-violet-600 border bg-white self-center justify-center px-8 py-1 rounded-[40px] max-md:px-5">Contact Us</button></li>
         </ul>
       </div>
