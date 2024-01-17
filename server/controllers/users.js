df7c07dadce4ece6b8722f90b9f775d8fde2a9fe
Varinder Dhillon <98728916+Varinder-Dhillon0@users.noreply.github.com>
2024-01-17 23:19:27 +0530

    register route updated
    - added jwt sign

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,20 +1,45 @@
 const userModel = require("../models/users");
+const jose = require("jose");
 
 //register route for creating new user
 // !need to handle some errors
 
-const createUser = async(req,res) =>{
-    const {name , email , password} = req.body;
-    
-    if(!name || !email || !password){
-        res.json({error : "name ,email and password are required."})
+const createUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        res.json({ error: "name ,email and password are required." });
         return;
     }
 
-    const user = new userModel();
-    await user.save();
+    const userData = {
+        name: name,
+        email: email,
+        password: password,
+    };
 
-    res.status(201).json({success : "user created successfully."});
-} 
+    //saving userdata in database
+    try{
+        const user = new userModel(userData);
+        await user.save();
+    }catch(err){
+        console.error(err);
+        res.json({error : "error occured while creating an user."})
+    }
+    
+    //generate jwt token
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    const jwt_token = await new jose.JWT({ name: name, email: email })
+        .setExpirationTime("1d")
+        .sign(secret);
 
-module.exports = {createUser}
+    //setting cookie expiration to 1 day
+    const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
+    res.cookie(jwt, jwt_token, { expires: expirationDate });
+    res.status(201).json({ success: "user created successfully." });
+};
+
+
+
+module.exports = { createUser };
