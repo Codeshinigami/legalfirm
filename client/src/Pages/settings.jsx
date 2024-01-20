49003c6388cbcc9802865de3e3d33ee871b24e92
anshuljain8110 <116940602+anshuljain8110@users.noreply.github.com>
2024-01-20 13:41:15 +0530

    margin css corrected

--- client/src/Pages/settings.jsx
+++ client/src/Pages/settings.jsx
@@ -20,7 +20,7 @@ export default function Settings() {
         tailwnd = 'dark:bg-slate-800 dark:text-white dark:text-slate-400 '
     }
     return (
-        <div style={{height:'100%'}} className={tailwnd}>
+        <div style={{height:'92.3vh'}} className={tailwnd}>
             <label class=" relative inline-flex items-center cursor-pointer m-4">
                 <input type="checkbox" value="" class="sr-only peer" onChange={()=>{saveMode()}} />
                 <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
