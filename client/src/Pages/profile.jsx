f5a91809078da160bdbb5620d60858ddc1b34960
Varinder Dhillon <98728916+Varinder-Dhillon0@users.noreply.github.com>
2024-01-21 02:41:10 +0530

    fixes

--- client/src/Pages/profile.jsx
+++ client/src/Pages/profile.jsx
@@ -19,8 +19,16 @@ export default function Profile() {
             <div className="text-center">
 
                 <div className={tailwnd}>
-                    <div className="px-4 py-5 sm:px-6 flex mr-auto w-40 ml-auto">
+                    <div className="px-4 py-5 sm:px-6 flex items-center">
                         <img src={credentials.profile.picture ? credentials.profile.picture : "/photo.png"} alt="wdw" width='80px' className="rounded-full" />
+                        <div className="ml-3">
+                            <h3 className="text-lg leading-6 font-medium ">
+                                User database
+                            </h3>
+                            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                                Details and informations about user.
+                            </p>
+                        </div>
                     </div>
                     <div className="border-t border-gray-200">
                         <dl>
@@ -33,6 +41,14 @@ export default function Profile() {
                                 </dd>
                             </div>
                             <hr />
+                            {/* <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">
+                        Best techno
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        React JS
+                        </dd>
+                    </div> */}
                             <hr />
                             <div className="bg-gray-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-sm font-medium text-gray-500">
@@ -43,7 +59,23 @@ export default function Profile() {
                                 </dd>
                             </div>
                             <hr />
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    Salary
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                    $10,000
+                                </dd>
+                            </div>
                             <hr />
+                            <div className="bg-gray-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">
+                                    About
+                                </dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+To get social media testimonials like these, keep your customers engaged with your social media accounts by posting regularly yourself
+                                </dd>
+                            </div>
 
 
                             {/* <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -59,7 +91,8 @@ export default function Profile() {
                     </div>
                 </div>
                 <div>
-                    <button onClick={() => { setFormDisplay(!formDisplay) }} className={` ${localStorage.getItem('theme')=='0'?"bg-white text-black ":"dark:bg-slate-800 dark:text-white dark:text-slate-400"}  p-2 rounde`}>Partner With us</button>
+                    
+                    <button onClick={() => { setFormDisplay(!formDisplay) }} className={`${localStorage.getItem('theme')=='0'?"bg-white text-black ":"dark:bg-slate-800 dark:text-white dark:text-slate-400"}  p-2 rounde`}>Partner With us</button>
                     {formDisplay ?
                         <form className="dark:bg-slate-900  dark:text-whit dark:text-slate-400 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-lg shadow-md">
                             <div class="grid gap-6 mb-6 md:grid-cols-2 mx-32">
@@ -71,8 +104,7 @@ export default function Profile() {
                                     <label for="last_name" class="block mb-2 text-sm font-medium  ">Last name</label>
                                     <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
                                 </div>
-
-                                <div>
+<div>
                                     <label for="website" class="block mb-2 text-sm font-medium  ">Location</label>
                                     <input type="text" id="website" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="flowbite.com" required />
                                 </div>
