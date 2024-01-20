6995b0c426d082d54630c1ad8c3b0c811b768c8b
anshuljain8110 <116940602+anshuljain8110@users.noreply.github.com>
2024-01-21 00:45:15 +0530

    text

--- client/src/Pages/profile.jsx
+++ client/src/Pages/profile.jsx
@@ -96,25 +96,25 @@ export default function Profile() {
                         <form>
                             <div class="grid gap-6 mb-6 md:grid-cols-2 mx-32">
                                 <div>
-                                    <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 ">First name</label>
+                                    <label for="first_name" class="block mb-2 text-sm font-medium  ">First name</label>
                                     <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
                                 </div>
                                 <div>
-                                    <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 ">Last name</label>
+                                    <label for="last_name" class="block mb-2 text-sm font-medium  ">Last name</label>
                                     <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
                                 </div>
 
                                 <div>
-                                    <label for="website" class="block mb-2 text-sm font-medium text-gray-900 ">Location</label>
+                                    <label for="website" class="block mb-2 text-sm font-medium  ">Location</label>
                                     <input type="text" id="website" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="flowbite.com" required />
                                 </div>
                                 <div>
-                                    <label for="visitors" class="block mb-2 text-sm font-medium text-gray-900 "> Mobile</label>
+                                    <label for="visitors" class="block mb-2 text-sm font-medium  "> Mobile</label>
                                     <input type="number" id="visitors" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
                                 </div>
                             </div>
                             <div class="mb-6 mx-32">
-                                <label class="block mb-2 text-sm font-medium text-gray-900 ">Lawyer Id</label>
+                                <label class="block mb-2 text-sm font-medium  ">Lawyer Id</label>
                                 <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
                             </div>
 
